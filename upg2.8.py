6df688b081bc2b621dd6597950f11333b4aeaf24
Gustav-NTI <gustav.hornlund@elev.ga.ntig.se>
2025-09-12 13:36:41 +0200

    Lektion tp 12/9

--- upg2.8.py
+++ upg2.8.py
@@ -1,6 +1,14 @@
-import random
-print ('Tärningarna är kastad')
-n1 = random.randint (1,6)
-n2 = random.randint (1,6)
-print ('Du fick' , n1,'och',n2 )
-print ('Summan är' , n1+n2)
+svar = "Ja"
+while svar == "Ja":
+    import random
+    print ('Tärningarna är kastad')
+    n1 = random.randint (1,6)
+    n2 = random.randint (1,6)
+    print ('Du fick' , n1,'och',n2 )
+    print ('Summan är' , n1+n2)
+    print ('Vill du kasta igen')
+    svar=input ('Ja/Nej: ')
+    if svar=='Nej':
+        break
+
+        
